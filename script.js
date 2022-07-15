a4d52be4615eebed6859f98b49221fb808bc1c66
Jepse <35108098+Jepse@users.noreply.github.com>
2022-07-15 11:39:39 -0500

    Base Idea

---
+++ script.js
@@ -0,0 +1,17 @@
+const THREE = require('three');
+"use strict";
+const $ = document.querySelectorAll.bind(document);
+$(".focusable, .button").forEach(el => {
+    el.addEventListener("mousedown", e => e.preventDefault());
+    el.setAttribute("tabindex", "0");
+});
+
+$(".server").forEach(el => {
+    el.addEventListener("click", () => {
+        const activeServer = $(".server.active")[0];
+        activeServer.classList.remove("active");
+        activeServer.removeAttribute("aria-selected");
+        el.classList.add("active");
+        el.setAttribute("aria-selected", true);
+    });
+});
